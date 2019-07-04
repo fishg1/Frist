b7386481190f6c9e4aa70876f67f6c0e8765cd91
fishg1 <52516326+fishg1@users.noreply.github.com>
2019-07-03 19:11:16 -0700

    Add files via upload

---
+++ main/java/fishg/frist/Frist.java
@@ -0,0 +1,62 @@
+package fishg.frist;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import lists.ItemList;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("frist")
+public class Frist 
+{
+	public static Frist instance;
+	public static final String modid = "frist";
+	private static final Logger logger = LogManager.getLogger(modid);
+	
+	public Frist() 
+	{
+		instance = this;
+		
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	private void setup(final FMLCommonSetupEvent event)
+	{
+		logger.info("Setup method registered.");
+	}
+	
+	private void clientRegistries(final FMLClientSetupEvent event)
+	{
+		logger.info("Client Registries method registered.");
+	}
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents
+	{
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event)
+		{
+			event.getRegistry().registerAll
+			(
+				ItemList.frospal = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("frospal"))
+			);		
+			logger.info("Items registered.");
+		}
+		
+		private static ResourceLocation location(String name)
+		{
+			return new ResourceLocation(modid, name);
+		}
+	}
+}
---
+++ main/java/lists/ItemList.java
@@ -0,0 +1,8 @@
+package lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList 
+{
+	public static Item frospal;
+}
